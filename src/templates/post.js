b4952134c98d15193c08beb92b168485b4dda7c8
Blake Adams <sbadams662@gmail.com>
2019-06-25 07:34:48 -0500

    Changing related posts thumbnail max width.

--- src/templates/post.js
+++ src/templates/post.js
@@ -190,7 +190,7 @@ export default ({ data }) => {
         edges {
             node {
                 ...MarkdownFields
-                ...PostFrontmatter
+                ...PostItemFrontmatter
             }
         }    
       }
